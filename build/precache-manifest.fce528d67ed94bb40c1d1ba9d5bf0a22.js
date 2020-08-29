92785d4fdde1354fa1a55f0360f4bf26a808ba21
FedericoTartarini <federicotartarini@gmail.com>
2020-08-29 17:57:45 +0800

    created build folder

---
+++ build/precache-manifest.fce528d67ed94bb40c1d1ba9d5bf0a22.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00bdca9e8a40491814a9bb193be9e971",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a078c762dbaf0c057853",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "61739a3bf59f0ed248ad",
+    "url": "/static/css/main.35c1d6a9.chunk.css"
+  },
+  {
+    "revision": "a078c762dbaf0c057853",
+    "url": "/static/js/2.f6e1e9c7.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/static/js/2.f6e1e9c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61739a3bf59f0ed248ad",
+    "url": "/static/js/main.97377187.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "198e98b7b75fa51fee622b12a3ed0219",
+    "url": "/static/media/me.198e98b7.jpg"
+  },
+  {
+    "revision": "393f2edda4d55648e927cf8b2925efad",
+    "url": "/static/media/me.393f2edd.webp"
+  },
+  {
+    "revision": "747ba43dbecd9f68e7d615a5084040e8",
+    "url": "/static/media/mosquito.747ba43d.png"
+  }
+]);
