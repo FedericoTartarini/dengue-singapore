9a26029c53675d96165d9cbe0a5fb28636b33115
FedericoTartarini <federicotartarini@gmail.com>
2020-08-30 15:17:02 +0800

    fixed errror sh file

--- python/run_python.sh
+++ python/run_python.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 # pull request to update files
-echo "Pull request"
+echo "cd into repo"
+cd /home/ubuntu/dengue-singapore
 
+echo "Pull request"
 git pull
 
 echo "Starting python scripts"
-sudo python3 download_dengue_data.py
+sudo python3 /python/download_dengue_data.py
 
 echo "Pushing to github"
 git add .
