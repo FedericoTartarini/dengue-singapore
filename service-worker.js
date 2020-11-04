602b07784f7924cfaf7a3b09a0dcac95283fa55a
FedericoTartarini <40018640+FedericoTartarini@users.noreply.github.com>
2020-11-04 22:02:04 +0000

    Deploying to gh-pages from  @ e2735df03240ddcf309f49b193fd3bbb7bb66449 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d6bf42b69731d3e8d7048621f7b3a000.js"
+  "/precache-manifest.9b6697c6885362a088eb65ad0e40c856.js"
 );
 
 self.addEventListener('message', (event) => {
